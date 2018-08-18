b2bca12009c667a2d150720fd496d9fac61b315c
Arsheen Khatib <arsheen.khatib@gmail.com>
2018-08-17 23:16:17 -0700

    Update correct_parenthesis.py

--- Data structures/stacks/correct_parenthesis.py
+++ Data structures/stacks/correct_parenthesis.py
@@ -1,3 +1,11 @@
+#Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+#An input string is valid if:
+
+#Open brackets must be closed by the same type of brackets.
+#Open brackets must be closed in the correct order.
+#Note that an empty string is also considered valid.
+
 class Stack:
     def __init__(self):
         self.stack = []
@@ -33,6 +41,9 @@ def parentheses(str1):
 
     stack = Stack()
     pairs = {'}': '{', ')': '(', ']': '['}
+    
+    if str1 == "":
+    return True
 
     for i in str1:
         if i in ('{', '[', '('):
