cc98fdd2a0c6ceac77d2576d0cdc6fab43a6a405
Arsheen Khatib <arsheen.khatib@gmail.com>
2018-09-12 11:49:34 -0700

    Update insertion_sort.py

--- Data structures/Sorting/sorting/insertion_sort.py
+++ Data structures/Sorting/sorting/insertion_sort.py
@@ -1,5 +1,9 @@
 # Insertion sort
 # For each element considered, put it in the correct order with respect to the other elements
+# Outer loop has n-1 iterations, Inner loop has 0 - i iterations
+# When data is in sorted order -> n-1 comaprisons, 2(n-1) moves -> O(n) complexity
+# When data is in reverse sorted order -> i comparisons (i.e; n(n-1)/2) and i+2 moves -> O(n**2) complexity
+
 
 def insertion_sort(array):
     for i in range(1, len(array)):
